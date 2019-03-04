d78b14e1fd6b4c09f229dd7aea9356eabaa06619
Eric (Martin) Mancini <coelomate@gmail.com>
2019-03-03 21:47:32 -0500

    simple proof of principle API call

--- apps/esi_api/lib/esi_api.ex
+++ apps/esi_api/lib/esi_api.ex
@@ -1,18 +1,34 @@
 defmodule EsiApi do
   @moduledoc """
-  Documentation for EsiApi.
+  An application to handle requests for information from the Eve: Online ESI API.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EsiApi.hello()
-      :world
-
-  """
+  @spec hello() :: atom()
   def hello do
     :world
+  end
+
+  def plex_price do
+    # PLEX type_id = 44992
+    {:ok, all_prices} = request("https://esi.evetech.net/latest/markets/prices/?datasource=tranquility")
+
+    plex_price_info =
+      all_prices
+      |> Poison.decode!()
+      |> Enum.filter(fn x -> x["type_id"] == 44992 end) # PLEX type_id = 44992
+      |> Enum.at(0)
+
+    plex_price_info["average_price"]
+  end
+
+  defp request(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:error, "404 Not Founf"}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
   end
 end
--- apps/webapp/lib/webapp_web/controllers/page_controller.ex
+++ apps/webapp/lib/webapp_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule WebappWeb.PageController do
   use WebappWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> assign(:name, "Eric Mancini")
+    |> assign(:plex_price, EsiApi.plex_price())
+    |> render("index.html")
   end
 end
+
