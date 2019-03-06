eba47fdd91020d33f54252e170e91edcfc99c0bc
Eric (Martin) Mancini <coelomate@gmail.com>
2019-03-05 22:02:36 -0500

    jesus take the wheel

--- apps/webapp/config/prod.exs
+++ apps/webapp/config/prod.exs
@@ -11,11 +11,21 @@ use Mix.Config
 # before starting your production server.
 config :webapp, WebappWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "polar-atoll-75486.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# TODO ERIC: Configure your database, something like this --
+config :webapp, Webapp.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+http: [port: System.get_env("PORT")]
 
 # ## SSL Support
 #
@@ -68,4 +78,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs" # removed for heroku
