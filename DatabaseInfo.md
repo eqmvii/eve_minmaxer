08944e92a4f7e9e4609656886e3b62e40bcaf205
Eric (Martin) Mancini <coelomate@gmail.com>
2019-04-06 12:04:06 -0400

    cleanup db actually working

---
+++ DatabaseInfo.md
@@ -0,0 +1,24 @@
+# Scale app up / down
+
+Down to kill: heroku ps:scale web=0
+Up to alive: heroku ps:scale web=1
+
+# Local setup info:
+
+Had to install and update PATH: file:///C:/Program%20Files/PostgreSQL/11/bin/
+
+# Info on current database
+
+heroku pg:ps
+
+# More goodness
+
+heroku pg:kill
+
+stop processes
+
+# Direct DB connect:
+
+heroku pg:psql
+
+raw commands
--- apps/webapp/priv/repo/cleanup_item_uris.exs
+++ apps/webapp/priv/repo/cleanup_item_uris.exs
@@ -7,10 +7,18 @@ Ecto.Adapters.SQL.query!(
   "DELETE FROM prices
     WHERE type_id in (
       SELECT type_id FROM items WHERE (name ILIKE '%20%' OR name ILIKE '%=%')
-    );", []
+    );",
+  [])
+
+Ecto.Adapters.SQL.query!(
+  Webapp.Repo, "DELETE FROM items WHERE (name ILIKE '%20%' OR name ILIKE '%=%');", []
 )
 
-#  mix run apps/webapp/priv/repo/cleanup_item_uris.exs
+# mix run apps/webapp/priv/repo/cleanup_item_uris.exs
 # heroku run mix run apps/webapp/priv/repo/cleanup_item_uris.exs
 
-# Worked but threw some errors about multiple connections along the way idk
+# Did not seem to work; errors:
+
+# 15:45:31.233 [error] Postgrex.Protocol
+# (#PID<0.317.0>) failed to connect: ** (Postgrex.Error) FATAL 53300 (too_many_connections)
+# too many connections for role "zdohngdcffzazj"
