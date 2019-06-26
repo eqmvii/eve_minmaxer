d34e228092c4a5a919a22fb05d8274cd66e59c2c
Eric (Martin) Mancini <coelomate@gmail.com>
2019-06-25 21:20:43 -0400

    working ExVCR test!

--- apps/esi_api/test/esi_api_test.exs
+++ apps/esi_api/test/esi_api_test.exs
@@ -1,13 +1,28 @@
 defmodule EsiApiTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
   doctest EsiApi
+
+  setup_all do
+    HTTPoison.start
+  end
+
+  setup do
+    ExVCR.Config.cassette_library_dir("fixture/vcr_cassettes")
+
+    :ok
+  end
 
   # TODO ERIC more of this
   test "greets the world" do
     assert EsiApi.hello() == :world
   end
 
+  # TODO ERIC more robust test here
   test "plex_price/0 returns the current plex price" do
-    assert EsiApi.plex_price()
+    use_cassette "example_api_hit" do
+      assert EsiApi.plex_price()
+    end
   end
 end
